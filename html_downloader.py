991ab0e2972811d9d107be896ec438fa1cf8769f
Dmitri Kovner <44140060+TheMinikit@users.noreply.github.com>
2022-04-02 23:42:14 +0300

    Add files via upload

--- html_downloader.py
+++ html_downloader.py
@@ -1,10 +1,15 @@
 import requests
 
 
-def fetch_pages(passed_url):
-    url_list = [passed_url]
+def fetch_pages(passed_urls, name="url"):
+
+    if type(passed_urls) != "list":
+        url_list = [passed_urls]
+    else:
+        url_list = passed_urls
+
     for url in url_list:
         response = requests.get(url)
         page_content = response.text
-        with open(str(url) + ".html", 'w', encoding="utf8") as fp:
+        with open(str(name) + ".html", 'w', encoding="utf8") as fp:
             fp.write(page_content)
